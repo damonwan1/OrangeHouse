db5e515392cb348b3dfde2af8b43528bb035cd3f
Meng Wan <mw1a17@soton.ac.uk>
2017-11-19 23:40:36 +0800

    添加项目文件。

---
+++ OrangeHouse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrangeHouse.Startup))]
+namespace OrangeHouse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
